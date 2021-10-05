63265175ee2397f69509dacb7aa6aa1941d01711
paucath <88663722+paucath@users.noreply.github.com>
2021-10-05 13:35:11 +0200

    jour 1

---
+++ src/Form/RegisterType.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre prénom',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre prenom'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre email',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre email'
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Votre mot de passe',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre mot de passe'
+                ]
+            ])
+            ->add('password_confirm', PasswordType::class, [
+                'label' => 'Confirmer votre mot de passe',
+                'mapped'=> false,
+                'attr' => [
+                    'placeholder' => 'Merci de confirmer votre mot de passe'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Inscription',
+                
+            ])
+
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
